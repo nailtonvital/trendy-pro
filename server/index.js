c66d77d17078019940e179f24049d81b43d79a2f
Nailton Vital <nailtonvital35@gmail.com>
2022-06-11 19:26:55 -0300

    Movie Controller
    Get trending movies from TMDB

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const res = require('express/lib/response');
 const server = express();
 const googleTrends = require('../server/controllers/GoogleTrendsControllers')
+const tmdb = require('../server/controllers/MovieController')
 
 server.use(cors());
 
 server.use(express.json());
+
+// Google Trends Area
 
 server.get('/relatedTopics', googleTrends.getRelatedTopics);
 
@@ -15,6 +18,17 @@ server.get('/relatedQueries', googleTrends.getRelatedQueries);
 server.get('/dailyTrends', googleTrends.getDailyTrends);
 
 server.get('/interestOverTime', googleTrends.getInterestOverTime);
+
+// Movies Area
+// Trending Today
+server.get("/trendingMoviesToday", tmdb.getTrendingMoviesDay)
+server.get("/trendingTVsToday", tmdb.getTrendingTvShowsDay)
+
+// Trending This Week
+server.get("/trendingMoviesWeek", tmdb.getTrendingMoviesWeek)
+server.get("/trendingTVsWeek", tmdb.getTrendingTvShowsWeek)
+
+// server.get("/trendingPersons", tmdb.getTrendingPersons)
 
 server.listen(3333, (err) => {
     if (!err) {
