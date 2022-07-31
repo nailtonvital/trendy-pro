d0e9c6ae86c84a4d2812a01b850e9d813dd06882
Nailton Vital <nailtonvital35@gmail.com>
2022-07-31 11:36:55 -0300

    adjusts

--- server/controllers/FacebookInterests.js
+++ server/controllers/FacebookInterests.js
@@ -1,22 +1,25 @@
 import { FacebookAdsApi } from 'facebook-nodejs-business-sdk';
+import dotenv from 'dotenv'
+dotenv.config({ silent: true });
+console.log('env : ', process.env);
 
-const access_token = 'EAAGa9VLMH9oBACV0CVclSl6Nqsj5qtlHZAEG3DGvCF98U0oPDAxRUFAoaRA1ZA9L6mQ0tBp4BlbTMtspOQ79t8tjYNQWZBnL2aU3BdJ163DYMVF1NXvZBKI9zZBoBZBX8Od3Bjt4ZBhBjagLLKEbsklV9PssXF6OEjwB9vdAla43gf5Bq2McObbCkISpLhUtwP42vp0GO4WAwZDZD';
+const access_token = process.env.FACEBOOK_ACESS_TOKEN;
 
-const api = FacebookAdsApi.init(access_token);
-const showDebugingInfo = true; // Setting this to true shows more debugging info.
-if (showDebugingInfo) {
-    api.setDebug(true);
-}
+// const api = FacebookAdsApi.init(access_token);
+// const showDebugingInfo = true; // Setting this to true shows more debugging info.
+// if (showDebugingInfo) {
+//     api.setDebug(true);
+// }
 
-const params = {
-    'type': 'adinterest',
-    'q': 'Golf',
-    'limit': '10000',
-    'locale': 'pt_BR',
-};
+// const params = {
+//     'type': 'adinterest',
+//     'q': 'Golf',
+//     'limit': '10000',
+//     'locale': 'pt_BR',
+// };
 
-api.call('GET', ['search'], params).then((response) => {
-    console.log(response.data)
-}).catch(error => {
-    console.log("something bad happened somewhere", error);
-});
+// api.call('GET', ['search'], params).then((response) => {
+//     console.log(response.data)
+// }).catch(error => {
+//     console.log("something bad happened somewhere", error);
+// });
