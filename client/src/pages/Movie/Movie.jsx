6a0f69ddb3a47374475c57c2a851dca80d1f5846
Nailton Vital <nailtonvital35@gmail.com>
2022-10-03 22:44:30 -0300

    chart adjusts

--- client/src/pages/Movie/Movie.jsx
+++ client/src/pages/Movie/Movie.jsx
@@ -19,31 +19,44 @@ export default function Movie() {
   const [topics, setTopics] = useState([]);
   const [error, setError] = useState([]);
 
+  const interestData = [{ "time": "Jan 2 – 8, 2022", "value": [88] }, { "time": "Jan 9 – 15, 2022", "value": [82] }, { "time": "Jan 16 – 22, 2022", "value": [78] }, { "time": "Jan 23 – 29, 2022", "value": [88] }, { "time": "Jan 30 – Feb 5, 2022", "value": [76] }, { "time": "Feb 6 – 12, 2022", "value": [77] }, { "time": "Feb 13 – 19, 2022", "value": [76] }, { "time": "Feb 20 – 26, 2022", "value": [83] }, { "time": "Feb 27 – Mar 5, 2022", "value": [76] }, { "time": "Mar 6 – 12, 2022", "value": [77] }, { "time": "Mar 13 – 19, 2022", "value": [90] }, { "time": "Mar 20 – 26, 2022", "value": [85] }, { "time": "Mar 27 – Apr 2, 2022", "value": [87] }, { "time": "Apr 3 – 9, 2022", "value": [87] }, { "time": "Apr 10 – 16, 2022", "value": [86] }, { "time": "Apr 17 – 23, 2022", "value": [84] }, { "time": "Apr 24 – 30, 2022", "value": [86] }, { "time": "May 1 – 7, 2022", "value": [82] }, { "time": "May 8 – 14, 2022", "value": [100] }, { "time": "May 15 – 21, 2022", "value": [88] }, { "time": "May 22 – 28, 2022", "value": [78] }, { "time": "May 29 – Jun 4, 2022", "value": [74] }, { "time": "Jun 5 – 11, 2022", "value": [68] }, { "time": "Jun 12 – 18, 2022", "value": [65] }, { "time": "Jun 19 – 25, 2022", "value": [69] }, { "time": "Jun 26 – Jul 2, 2022", "value": [65] }, { "time": "Jul 3 – 9, 2022", "value": [96] }, { "time": "Jul 10 – 16, 2022", "value": [75] }, { "time": "Jul 17 – 23, 2022", "value": [67] }, { "time": "Jul 24 – 30, 2022", "value": [72] }, { "time": "Jul 31 – Aug 6, 2022", "value": [68] }, { "time": "Aug 7 – 13, 2022", "value": [99] }, { "time": "Aug 14 – 20, 2022", "value": [85] }, { "time": "Aug 21 – 27, 2022", "value": [77] }, { "time": "Aug 28 – Sep 3, 2022", "value": [79] }, { "time": "Sep 4 – 10, 2022", "value": [89] }, { "time": "Sep 11 – 17, 2022", "value": [81] }, { "time": "Sep 18 – 24, 2022", "value": [94] }, { "time": "Sep 25 – Oct 1, 2022", "value": [97] }]
+
+  const time = interestData.map(item=>{
+    return item.time
+  })
+
+  const valueData = interestData.map(item => {
+    return item.value[0]
+  })
+
   const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: time,
     datasets: [
       {
-        label: "First dataset",
+        label: "Mob Psycho 100",
         lineTension: 0.2,
-        data: [33, 53, 85, 41, 44, 65],
+        data: valueData,
         fill: true,
         backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)",
+        borderColor: "white",
         
-      },
-      {
-        label: "Second dataset",
-        lineTension: 0.2,
-        data: [33, 25, 35, 51, 54, 76],
-        fill: false,
-        borderColor: "#742774"
       }
     ]
   };
+
+  var options = {
+    scales: {
+      xAxis: {
+        ticks: {
+          maxTicksLimit: 8.1
+        }
+      }
+    }
+  }
   
 
   useEffect(() => {
-    api.get("/anime/20850")
+    api.get("/anime/140439")
     .then((response) => {
       setMovie(response.data.data.Media)
     })
@@ -213,7 +226,7 @@ export default function Movie() {
 
             <h5>Interest Over Time</h5>
             <div className={style.interest}>
-              <Line data={data}/>
+              <Line data={data} options={options} />
             </div>
           </div>
         </div>
