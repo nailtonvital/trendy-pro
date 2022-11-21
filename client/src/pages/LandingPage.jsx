0ee0b13b94904cca87eaf3034e48c317cb6e1927
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-11-21 21:58:23 +0000

    py

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -61,7 +61,7 @@ export default function LandingPage() {
                 We track every topic across the internet to identify growing
                 trends.
               </p>
-              <div className="text-3xl">
+              <div className="text-3xl py-5">
                 <a href="/Login" class="relative inline-flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group">
                   <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-12 absolute left-0 top-0 bg-white opacity-[3%]"></span>
                   <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-60 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
