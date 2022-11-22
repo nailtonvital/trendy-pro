70b8d9671442dd2992b55da1998ef981980d7fca
Lucas Genú <99687526+LucasGenu@users.noreply.github.com>
2022-11-22 02:30:27 +0000

    Home 1/3

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import stars from '../assets/stars.png'
+import minino from '../assets/minino.webp'
 
 export default function Home() {
   document.title = 'TrendyPro | Home'
   return (
     <div>
-      <div className="bg-gradient-to-r flex from-purple-800 to-pink-700 h-48 rounded-xl w-full text-white font-sans p-4">
+      <div className="bg-gradient-to-r flex from-purple-800 to-pink-700 h-48 rounded-xl w-full text-white font-sans p-0 pt-1 mt-4">
+        <img src={minino}/>
         <div className="mt-9 ml-4">
           <p className="text-[#CBCBCB] font-bold">Good Evening</p>
           <h2 className="text-4xl font-bold">Nailton Vital</h2>
@@ -13,15 +15,20 @@ export default function Home() {
         <img src={stars} className="h-28" alt="" srcset="" />
       </div>
 
-      <div role="status" class="max-w-sm animate-pulse">
-        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-        <span class="sr-only">Loading...</span>
+<div className='items-stretch flex space-x-10'>
+      <div className="bg-[#1B1A20] h-96 rounded-xl w-1/2 mt-7">
+      <p className="text-white text-center ">CAROUSEL</p>
       </div>
+
+      <div className="bg-[#1B1A20] h-96 rounded-xl w-1/4 mt-7">
+      </div>
+
+      <div className="bg-[#1B1A20] h-96 rounded-xl w-1/4 mt-7">
+      </div>
+
+</div>
+
     </div>
+
   );
 }
