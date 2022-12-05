2dfe1fccf5137d402e381cd402a7f4d9dd141c8e
Lucas Genú <99687526+LucasGenu@users.noreply.github.com>
2022-12-05 03:05:05 +0000

    ACABO?

--- client/src/pages/RelatedQueries.jsx
+++ client/src/pages/RelatedQueries.jsx
@@ -44,7 +44,7 @@ export default function RelatedQueries() {
               ref={inputRef}
               onChange={handleChange}
               type="search"
-              className="form-control rounded-full flex-auto min-w-0 block w-full px-5 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="form-control rounded-full flex-auto min-w-0 block w-full px-5 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700  focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Type a keyword"
               aria-label="Search"
               aria-describedby="button-addon2"
